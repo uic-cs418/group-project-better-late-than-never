ed64979d9f0cbe07a89b12e4f4986643f1db669d
chasty2 <email@codyhasty.net>
2025-04-03 23:30:13 -0500

    Revert changes to avg_score_calculation

--- review_score_analysis.py
+++ review_score_analysis.py
@@ -3,17 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def calculate_average_review_score(restaurants_df, reviews_df):
+def calculate_average_review_score(reviews_df):
     """
-    Input filtered reviews and restaurants dataframes, return a dataframe with the average review score for each 
-    restaurant in column 'avg_review_score' appended to restaurant dataframe
+    Input filtered reviews dataframes return a dataframe with the average review score for each 
+    restaurant in column 'avg_review_score' appended to reviews dataframe
     """
     avg_review_scores = reviews_df.groupby('business_id')['stars'] \
                                         .mean() \
                                         .reset_index() \
                                         .rename(columns={'stars': 'avg_review_score'})
 
-    merged_data = pd.merge(restaurants_df, avg_review_scores, on='business_id', how='left')
+    merged_data = pd.merge(reviews_df, avg_review_scores, on='business_id', how='left')
 
     return merged_data
 
