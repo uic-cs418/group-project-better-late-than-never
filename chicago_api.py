a177e8f137fd5e148d28e7fdf16b6cd961121398
chasty2 <email@codyhasty.net>
2025-04-23 22:36:24 -0500

    Return unique Chicago neighborhoods

--- chicago_api.py
+++ chicago_api.py
@@ -7,11 +7,11 @@ def read_api_key(path="data/api_key_yelp.txt"):
         return file.read().strip()
 
 def read_neighborhoods(path="data/chicago_neighborhoods.txt"):
-    neighborhoods = []
+    neighborhoods = set()
     with open(path, "r") as file:
         for line in file:
-            neighborhoods.append(line.strip())
-    return neighborhoods
+            neighborhoods.add(line.strip())
+    return list(neighborhoods)
 
 def location_search_params(api_key, location, **kwargs):
     url = "https://api.yelp.com/v3/businesses/search"
