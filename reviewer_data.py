6954e2d75e9a40198bc4a8df2c7723c2ea66c52e
Cody Hasty <36090014+chasty2@users.noreply.github.com>
2025-05-07 18:23:23 -0500

    Final draft (#19)
    * Update title of graph
    
    * Formatting
    
    * Final draft

--- reviewer_data.py
+++ reviewer_data.py
@@ -2,18 +2,19 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
+
 def averageRatingbyUsers(users_df, reviews_df):
     """
     histogram of average ratings given by users
     """
     avgStarUser = reviews_df.groupby("user_id")["stars"].mean().reset_index()
     avgStarUser.columns = ["user_id", "avgRatingGiven"]
-    
+
     possiblebias = users_df.merge(avgStarUser, on="user_id", how="inner")
 
     plt.figure(figsize=(10, 6))
     sns.histplot(possiblebias["avgRatingGiven"], bins=20)
-    plt.title("Distribution of Average Ratings per User. Do Users Rate Too Generously?")
+    plt.title("Distribution of Average Ratings per User")
     plt.xlabel("Average Rating by User")
     plt.ylabel("Number of Users")
     plt.grid(True)
@@ -25,11 +26,14 @@ def numReviewDistributionbyUsers(users_df):
     groups users by number of reviews written
     """
     bins = [0, 2, 5, 10, 20, 50, 100, 500, float("inf")]
-    labels = ["1-2","3-5","6-10","11-20","21-50","51-100","101-500","500+"]
+    labels = ["1-2", "3-5", "6-10", "11-20", "21-50", "51-100", "101-500", "500+"]
 
     users_df["review_category"] = pd.cut(
-        users_df["review_count"], bins=bins, labels=labels,
-        right=False, include_lowest=True
+        users_df["review_count"],
+        bins=bins,
+        labels=labels,
+        right=False,
+        include_lowest=True,
     )
     review_counts = users_df["review_category"].value_counts().sort_index()
 
